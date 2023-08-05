e09d0976f3e99824e25135492b485b1dbce7e9d2
sagmadan <sagarmadan5@gmail.com>
2023-08-05 15:30:07 +0530

    Keeping the access open for all signups

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,14 +12,10 @@ const loginUser = async (req, res) => {
   try {
     const user = await User.login(username, password)
 
-    // restricting access to only authorized users
-    if (user.isAuthorized) {
-        // create a token
-        const token = createToken(user._id)
+    // create a token
+    const token = createToken(user._id)
 
-        res.status(200).json({username, token})
-    }
-    res.status(401).json({error: "User is unauthorized"})
+    res.status(200).json({username, token})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
@@ -32,14 +28,10 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(username, password)
 
-    // restricting access to only authorized users
-    if (user.isAuthorized) {
-        // create a token
-        const token = createToken(user._id)
+    // create a token
+    const token = createToken(user._id)
 
-        res.status(200).json({username, token})
-    }
-    res.status(401).json({error: "User is unauthorized"})
+    res.status(200).json({username, token})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
